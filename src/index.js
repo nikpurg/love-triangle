2d2fe64f69f37ee28308d45028fa8c39ccae6d17
NIkpurg <nIkpurg@gmail.com>
2019-03-18 16:17:20 +0300

    feat: add triangle prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+// /**
+//  * @param preferences - an array of integers. Indices of people, whom they love
+//  * @returns number of love triangles
+//  */
+// module.exports =
+ function getLoveTrianglesCount(preferences = [2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4]) {
   // your implementation
+for (var i=0; i < preferences.length; i++){
+    if  ((preferences[i] == i-1)){
+     var k = k++; 
+    }
+
+}
+return k;
 };
+
+ console.log(getLoveTrianglesCount());
