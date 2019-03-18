4ad24ec0a9b35e2656eaabbaccc59b61ecde93c1
nikpurg <nikpurg@gmail.com>
2019-03-19 00:03:23 +0300

    fix: trangle random number

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-// /**
-//  * @param preferences - an array of integers. Indices of people, whom they love
-//  * @returns number of love triangles
-//  */
-// module.exports =
- function getLoveTrianglesCount(preferences = [2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4]) {
+/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
+module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-for (var i=0; i < preferences.length; i++){
-    if  ((preferences[i] == i-1)){
-     var k = k++; 
+  preferences.unshift(0);
+  var one;
+  var two;
+  var three;
+  var count = 0;
+  for (var i = 1; i < preferences.length; i++) {
+    one = preferences[i];
+    two = preferences[one];
+    three = preferences[two];
+
+    if (i == three) {
+
+      count = count++;
     }
 
-}
-return k;
+  }
+  return count / 3;
 };
 
- console.log(getLoveTrianglesCount());
+//  console.log(getLoveTrianglesCount());пше 
